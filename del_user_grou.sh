5252a0283b4b8c964f4835a5f67dd6a9865a1cab
huweikkk <17012140@qq.com>
2015-05-28 10:50:52 +0800

    add user_contron_script

---
+++ del_user_grou.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+userdel adm
+userdel lp
+userdel sync
+userdel shutdown
+userdel halt
+userdel news
+userdel uucp
+userdel operator
+userdel games
+userdel gopher
+userdel ftp
+groupdel adm
+groupdel lp
+groupdel dip
+
